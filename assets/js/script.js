3fcd562bab6d955df3ab68cffef3113e822e3a60
luis muciño <nievesmucino@gmail.com>
2023-08-03 17:38:11 -0600

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ console.log("Página en construcción con Patrick Star");
 function clickPatricio() {
     alert ("la mayonesa es un instrumento???")
 }
+
+function changeColor( elementHtml, color) {
+    alert( "Qué emoción, voy a cambiar mi color");
+    console.log(elementHtml);
+    console.log (color); 
+
+}
+elementHtml.style.color = color; 
